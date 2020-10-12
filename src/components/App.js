d4e4fc8b4f68575afcf574b37eb2a145f86a01fc
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-11 23:53:35 -0300

    configurado o react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const ProductList = () => <div>Lista de produtos</div>;
+const Checkout = () => <div>Checkout do produto</div>;
+
 
 const App = () => {
     return(
-        <div>App</div>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={ProductList} />
+                <Route path="/checkout" exact component={Checkout} />
+            </Switch>
+        </BrowserRouter>
     )
 }
 
