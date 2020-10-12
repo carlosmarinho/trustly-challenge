0c83ef511aa1998d09d42562a76c0eb6ac8374ef
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-12 14:30:59 -0300

    adicionado header

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from 'pages/home'
+import Header from './header';
 
 const Checkout = () => <div>Checkout do produto</div>;
 
@@ -8,6 +9,7 @@ const Checkout = () => <div>Checkout do produto</div>;
 const App = () => {
     return(
         <BrowserRouter>
+            <Header />
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/checkout" exact component={Checkout} />
