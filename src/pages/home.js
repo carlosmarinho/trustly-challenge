d5d94bc0e2c5e220cf5a7274fd0ce2490a623453
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-12 01:24:13 -0300

    exibindo a lista simples de produto

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,17 +2,27 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { fetchAllProducts } from 'actions/product';
+import product from 'json/product';
 
 const Home = () => {
-    const { productReducer } = useSelector(state => state)
+    const { productReducer: { products } } = useSelector(state => state)
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(fetchAllProducts());
     },[])
 
+    console.log("productreducer: ", products);
+
     return(
-        <div>Minha listagem de produtos</div>
+        <>
+            <div>Minha listagem de produtos</div>
+            <ul>
+                {products.map(product => 
+                    <li>{product.description}</li>
+                )}
+            </ul>
+        </>
     )
 }
 
