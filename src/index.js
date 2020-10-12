0f317b90f380abf0050caab9dbeb5167d6bf2017
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-12 00:13:09 -0300

    incluido configuração para inclusão de paths absoluto

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import reducers from './reducers';
+import reducers from 'reducers';
 
-import App from './components/App';
+import App from 'components/App';
 
 // const reducers = () => {};
 const store = createStore(
