c8712e3f5d6c7f9cb2f499d0e7ed9f429b6e7aaf
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-12 00:06:52 -0300

    incluido o middleware redux thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
 import App from './components/App';
 
 // const reducers = () => {};
 const store = createStore(
-    reducers
+    reducers,
+    applyMiddleware(reduxThunk)
 );
 
 ReactDOM.render(
