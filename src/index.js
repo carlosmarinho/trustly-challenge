4946e8f3edd5ea6297beaf48228fb0056842d6e6
Carlos Luiz Marinho da Silva Junior <cljunior@brasilseg.com.br>
2020-10-12 00:05:40 -0300

    incluido os reducers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 import App from './components/App';
 
-const reducers = () => {};
+// const reducers = () => {};
 const store = createStore(
     reducers
 );
